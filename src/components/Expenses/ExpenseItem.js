dfde0415576f818a4284f9b3bf8639998eaf5855
Danny <barakadan421@gmail.com>
2023-09-20 14:44:33 +0300

    update expenseItem js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,13 +8,12 @@ function ExpenseItem(props) {
     // console.log("Clicked");
   };
   return (
-    <div className="bg-[#2e2d2d] text-white cursor-pointer p-3 ">
+    <div className="bg-[#2e2d2d] text-white cursor-pointer p-3 mb-4 ">
       <div className="flex justify-between">
         <ExpenseDate date={props.date} />
         <div>
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="text-xl font-semibold mt-1">${props.amount}</div>
-          <button onClick={clickHandler}>change title</button>
         </div>
       </div>
     </div>
